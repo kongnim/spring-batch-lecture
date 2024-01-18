80dcd848c888d34850c21ada19e668be7cfe0bbb
kongnim <amabile29@naver.com>
2024-01-18 09:09:31 +0900

    Section1 강의 필기

--- src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
@@ -14,29 +14,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-@Configuration
-public class HelloJobConfiguration {
+@Configuration // 설정 클래스니까 Configuration 어노테이션을 선언해야 여기서 빈을 생성할 수 있다.
+public class HelloJobConfiguration { // Job을 정의한다? -> 아래 내용을 구성을 한다는 의미. 내용 자체가 잡을 실행할 수 있는 각각의 단위들을 설정하고 구성한다는 의미이다.
 
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
+    // Job을 편리하게 구성할수 있는 몇가지 유틸성의 클래스들을 제공한다.
+    // 예: JobBuilderFactory, StepBuilderFactory
+    private final JobBuilderFactory jobBuilderFactory; // Job을 생성하는 빌더팩토리.
+    private final StepBuilderFactory stepBuilderFactory; // Step을 생성하는 빌더팩토리.
+    // new job, new step 이런 식의 생성이 아닌.. BuilderFactory의 도움을 받아 쉽게 Job, Step을 생성할수 있도록 유틸성의 클래스를 제공
 
     @Bean
     public Job helloJob() {
-        return this.jobBuilderFactory.get("helloJob")
+        return this.jobBuilderFactory.get("helloJob") // Job 생성. jobBuilderFactory.get("이름부여") <= 이것 자체가 잡을 생성하는 설정.
                 .incrementer(new RunIdIncrementer())
-                .start(step1())
-                .next(step2())
-                .build();
+                .start(step1()) // 인자로 Step이 들어감. // Job은 필수 요소로 Step을 가지고 있어야 한다. 가장 먼저 출발할 스텝을 start에 넣어준다.
+                .next(step2()) // start 다음 스텝을 무엇으로 할 것이냐는 의미.
+                .build(); // job의 구현체를 생성.
     }
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
+        return stepBuilderFactory.get("step1") // step 생성
+                .tasklet(new Tasklet() { // step안에서 단일 task로 수행되는 로직을 구현. step 내에 tasklet 필수.
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step1 has executed");
-                        return RepeatStatus.FINISHED;
+                        System.out.println("step1 has executed"); // 비즈니스 로직이 tasklet안에 구현되어 있음.
+                        // 기본적으로 step은 tasklet을 무한반복시킨다.
+                        // 특별한 경우외에는 무한반복이 되면 안되기때문에 RepeatStatus를 리턴해준다.
+                        // return null은 한번 실행하고 종료.
+                        return RepeatStatus.FINISHED; // return null과 같은 의미를 갖는 RepeatStatus이다.
                     }
                 })
                 .build();
--- src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing
+@EnableBatchProcessing // Spring batch에 enable로 시작하는 배치가 많다. EnableWebMvc, EnableSpringSecurity,…
+// 보통 Enable로 시작하는 어노테이션은 그 기능이 초기화할수있도록 활성화하는 역할을 함.
+// 보통 메인 애플리케이션위에 선언
 public class SpringBatchLectureApplication {
 
     public static void main(String[] args) {
